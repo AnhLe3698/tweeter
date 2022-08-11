10a8f16282ebaceae2be681471fe86d84de6fd27
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-11 08:56:30 -0600

    Adding letter counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,19 @@
 $(document).ready(function() {
   // --- our code goes here ---
 
-  
+  //Keyup is the better choice since it can also check for alternate key presses
+  // like delete unlike keypress. Keydown misses the first character
+  $("#tweet-text").keyup(function() {
+    let lengthTweet = String($(this).val()).length;
+    console.log(lengthTweet);
+    let counter = $(this).parent().parent().children('.bottomBar').children('.counter');
+    counter.text(140 - lengthTweet);
+    if (Number(counter.text()) < 0) {
+      counter.css({"color": "red"});
+    } else {
+      counter.css({"color": "black"});
+    }
+    //$('.counter').text(140 - lengthTweet);
+  });
+
 });
