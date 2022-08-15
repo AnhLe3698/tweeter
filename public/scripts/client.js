4eeb3aa57f352fb304c8fe8ad6082ef1f98a1493
AnhLe3698 <anhviet.le00@hotmail.com>
2022-08-15 14:38:05 -0600

    Added a get Tweet request Jquery function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,28 @@
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 const renderTweets = function(tweets) {
 // loops through tweets
@@ -60,26 +60,25 @@ const createTweetElement = function(tweet) {
   return $tweet;
 };
 
-renderTweets(data);
+const loadTweets = function() {
+  $.getJSON('/tweets/', function(data) {
+    renderTweets(data);
+  });
+};
+
+//renderTweets(data);
+loadTweets();
 
 $("#newText").submit(function(event) {
   let data = $("#newText").serialize();
   
   event.preventDefault();
-
-  alert("Handler for .submit() called.");
   alert(`${data}`);
-  console.log(event);
-  // $.ajax('more-posts.html', { method: 'POST' })
-  //   .then(function (morePostsHtml) {
-  //     console.log('Success: ', morePostsHtml);
-  //   });
-  // let data = ($("#tweet-text").first().val()).serialize();
-  // alert("Handler for .submit() called.");
-  // event.preventDefault();
-  $.post('/', data)
+
+  $.post('/tweets/', data)
     .then(function() {
-      console.log('Success: ');
+      $.getJSON('/tweets/', function(data) {
+        renderTweets(data);
+      });
     });
-  //http://localhost:8080/
 });
